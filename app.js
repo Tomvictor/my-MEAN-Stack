9c6f3a619ea3aa23919ee531b8ef5b3e242ca4da
Tom Victor <vjtomvictor@icloud.com>
2017-03-12 21:32:24 +0530

    static file bug fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.engine('.html', require('ejs').__express);
 
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static('public'));
 app.set('view engine', 'html');
 
 // Dummy users
